986403009aefa6521cded135358d64f1f77297bf
Soumik Sen <mastersoumik2003@gmail.com>
2023-11-05 15:07:59 +0530

    Changes updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
         <div className="flex flex-col ">
           <button
             type="button"
-            className="w-5/12 mt-5  text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900  dark: bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="w-5/12 mt-5  text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-green-900  dark: bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
           >
             <a className="button" onClick={handleDownload}>
               Download Resume
@@ -50,6 +50,18 @@ const About = () => {
               target="_blank"
             >
               GitHub
+            </a>
+          </button>
+          <button
+            type="button"
+            className="w-4/12 mt-5  text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900  dark: bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            <a
+              className="button"
+              href="https://www.linkedin.com/in/soumik-sen-210473211/"
+              target="_blank"
+            >
+              Linkedin
             </a>
           </button>
         </div>
